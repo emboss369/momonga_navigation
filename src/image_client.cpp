3d8a1c8b407b513805d5c883d448f55428a98813
emboss369 <knd@hera.eonet.ne.jp>
2018-11-06 05:34:20 +0900

    image test

---
+++ src/image_client.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "draw_frames");
+  std::vector<std::string> frame_ids(argv + 1, argv + argc);
+  FrameDrawer drawer(frame_ids);
+  ros::spin();
+}
